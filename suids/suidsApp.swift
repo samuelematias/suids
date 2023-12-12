347d490602aa877e11033de8ee57e2ff1ed9f0e2
Samuel Matias <hi@samuelematias.com>
2023-12-12 10:46:41 -0300

    Initial Commit

---
+++ suids/suidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  suidsApp.swift
+//  suids
+//
+//  Created by samuelematias on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct suidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
